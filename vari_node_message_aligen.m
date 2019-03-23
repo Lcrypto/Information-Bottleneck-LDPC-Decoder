e1c44e97737a8eec22c421441517222c9163efae
BigBluBruin <lfwang@g.culda.edu>
2019-03-22 17:53:47 -0700

    1. add channel dist 2. channel distribution will not participate in the base LLR competition 3. channel distribution will participate in the next iteration

--- vari_node_message_aligen.m
+++ vari_node_message_aligen.m
@@ -1,4 +1,4 @@
-function [ new_Vmapping,pda_join_x_z,t_z_transform  ] = vari_node_message_aligen( Vmapping,node_distri,T )
+function [ new_Vmapping,pda_join_x_z,t_z_transform  ] = vari_node_message_aligen( Vmapping,channel_dist,node_distri,T )
 %UNTITLED4 Summary of this function goes here
 %   check node and message node are message aligen are seperated since the
 %   relashionship between mapping and node_distri are different
@@ -10,7 +10,7 @@ function [ new_Vmapping,pda_join_x_z,t_z_transform  ] = vari_node_message_aligen
 %%be acted as  pd(x,z) can its message will not be updated
 new_Vmapping=Vmapping;
 ave_LLR=zeros(1,length(node_distri));
-for ii=1:length(node_distri)
+for ii=2:length(node_distri)  
     if(node_distri(ii)~=0)
         match_mapping_index=ii-1;
         ave_LLR(ii)=sum(abs(log2(Vmapping(match_mapping_index).NProbJoinXT(1,:)./Vmapping(match_mapping_index).NProbJoinXT(2,:))))/T;
@@ -26,7 +26,11 @@ state_vec(base_degree)=1;
 for ii=1:length(node_distri)
     if(node_distri(ii)~=0&&ii~=base_degree)
         state_vec(ii)=2;     %Update state_vec
-        [ pd_join_x_z,t_z_transform(ii,:) ] = message_alignment( Vmapping(ii-1).NProbJoinXT,pda_join_x_z,T);
+        if ii==1 %%(channel observation)
+            [ pd_join_x_z,t_z_transform(ii,:) ] = message_alignment( channel_dist,pda_join_x_z,T);
+        else            
+            [ pd_join_x_z,t_z_transform(ii,:) ] = message_alignment( Vmapping(ii-1).NProbJoinXT,pda_join_x_z,T);
+        end
         [ pda_join_x_z ] = average_update( pd_join_x_z,pda_join_x_z,state_vec,node_distri );    %update Pda(X,Z)
         state_vec(ii)=1;    %%Since it has been aligned, stste is turned into 1
     end
